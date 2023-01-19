29a9d4b2b10db62f82ea1fc28b3b689d741fa0a3
jan <samjan1127@gmail.com>
2023-01-20 01:52:31 +0900

    refactor: 각도기 코드 수정

--- year2023/month01/Protractor19.java
+++ year2023/month01/Protractor19.java
@@ -4,11 +4,11 @@ public class Protractor19 {
     public int solution(int angle) {
         int answer = 0;
 
-        if (angle <= 180 && angle > 0) {
-            if (angle == 180) answer = 4;
-            else if (angle < 180 && angle > 90) answer = 3;
-            else if (angle == 90) answer = 2;
-            else answer = 1;
+        if (angle > 0 && angle <= 180) {
+            if (angle < 90) return 1;
+            else if (angle == 90) return 2;
+            else if (angle < 180) return 3;
+            else return 4;
         }
 
         return answer;
