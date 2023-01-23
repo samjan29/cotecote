aa136272b577c85eaf82961b6b818b3a893de4b0
jan <samjan1127@gmail.com>
2023-01-23 20:38:25 +0900

    add: 23/1/23

---
+++ year2023/month01/Day23_4.java
@@ -0,0 +1,13 @@
+package year2023.month01;
+
+public class Day23_4 {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int[] answer = new int[num2 - num1 + 1];
+
+        int idx = 0;
+        for (int i = num1; i <= num2; i++, idx++)
+            answer[idx] = numbers[i];
+
+        return answer;
+    }
+}
