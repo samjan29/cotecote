fbc480b64829bddcbd734d9f9717d315520359df
jan <samjan1127@gmail.com>
2023-01-26 23:51:40 +0900

    add: 26

---
+++ year2023/month01/Day26_2.java
@@ -0,0 +1,15 @@
+package year2023.month01;
+
+public class Day26_2 {
+    public int solution(int hp) {
+        int answer = 0;
+        int[] ants = {5, 3, 1};
+
+        for (int ant : ants) {
+            answer += hp / ant;
+            hp %= ant;
+        }
+
+        return answer;
+    }
+}
