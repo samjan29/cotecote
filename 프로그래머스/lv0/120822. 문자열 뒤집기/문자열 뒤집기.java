5ea669751214a48e2a7714af91e1569739bfadbe
samjan29 <78520058+samjan29@users.noreply.github.com>
2023-02-12 00:59:06 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.02 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(my_string);
+        sb.reverse();
+
+        return sb.toString();
+    }
+}
