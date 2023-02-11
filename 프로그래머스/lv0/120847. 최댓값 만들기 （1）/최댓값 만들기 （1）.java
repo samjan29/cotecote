4b6f241f407bc41cd2e47d215f6f3fa4f5af9ee1
samjan29 <78520058+samjan29@users.noreply.github.com>
2023-02-12 00:56:02 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.02 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] < numbers[j]) {
+                    int tmp = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = tmp;
+                }
+            }
+        }
+        
+        return numbers[0] * numbers[1];
+    }
+}
