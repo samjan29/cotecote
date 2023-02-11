621ac6ae9645d59d4faa5eeae3067cec39030419
samjan29 <78520058+samjan29@users.noreply.github.com>
2023-02-12 01:08:23 +0900

    [level 1] Title: 예산, Time: 0.10 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        
+        for (int i = 0; i < d.length; i++)
+            for (int j = i + 1; j < d.length; j++)
+                if (d[i] > d[j]) {
+                    int tmp = d[i];
+                    d[i] = d[j];
+                    d[j] = tmp;
+                }
+        
+        int sumD = 0;
+        while (sumD < budget) {
+            if (answer == d.length) break;
+            
+            sumD += d[answer++];
+        }
+        
+        return sumD > budget ? --answer : answer;
+    }
+}
