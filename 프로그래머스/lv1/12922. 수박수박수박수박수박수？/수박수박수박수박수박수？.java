38ee007ddf8bd19a2dcd907d49e60232181cbf0b
samjan29 <78520058+samjan29@users.noreply.github.com>
2023-05-08 23:23:11 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 15.94 ms, Memory: 81 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+
+        if (n % 2 == 0) {
+            answer = "수박".repeat(n / 2);
+        } else {
+            answer = "수박".repeat(n / 2) + "수";
+        }
+
+        return answer;
+    }
+}
