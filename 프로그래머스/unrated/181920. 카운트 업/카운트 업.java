763d5c678ccb211bc9a6c92078410c9bd3866e30
samjan29 <78520058+samjan29@users.noreply.github.com>
2023-05-11 21:48:49 +0900

    [unrated] Title: 카운트 업, Time: 0.02 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int start, int end) {
+        int[] answer = new int[end - start + 1];
+        
+        for(int i = 0; i < end - start + 1; i++) {
+            answer[i] = start + i;
+        }
+        
+        return answer;
+    }
+}
