00206c63bbbfb7d06ef57dfb1bb07ecabace545f
samjan29 <78520058+samjan29@users.noreply.github.com>
2023-05-13 21:35:42 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int a, int b) {
+        
+        if (a % 2 != 0 && b % 2 != 0) {
+            return a * a +  b * b;
+        } else if (a % 2 != 0 || b % 2 != 0) {
+            return 2 * (a + b);
+        } else {
+            return Math.abs(a - b);
+        }
+        
+    }
+}
