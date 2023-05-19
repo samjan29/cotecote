3d4b10b3c1d577b2aefaeb1ce0bfbbaa1b20aa0e
samjan29 <78520058+samjan29@users.noreply.github.com>
2023-05-19 21:34:56 +0900

    [unrated] Title: 접미사인지 확인하기, Time: 0.04 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String my_string, String is_suffix) {
+        int answer = 0;
+        
+        if (my_string.contains(is_suffix) && my_string.substring(my_string.length() - is_suffix.length()).equals(is_suffix)) {
+            answer = 1;
+        }
+        
+        return answer;
+    }
+}
