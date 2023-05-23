f81bfe4603bf655cd00852b5500b7d8fef21e686
samjan29 <78520058+samjan29@users.noreply.github.com>
2023-05-23 22:04:15 +0900

    [level 2] Title: 최솟값 만들기, Time: 1.22 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -6,8 +6,8 @@ class Solution
     {
         int answer = 0;
 
-        Arrays.sort(A);
-        Arrays.sort(B);
+        quickSort(A, 0, A.length - 1);
+        quickSort(B, 0, B.length - 1);
         
         for (int i = 0; i < A.length; i++) {
             answer += A[i] * B[B.length - i - 1];
@@ -15,4 +15,48 @@ class Solution
 
         return answer;
     }
+    
+    private void quickSort(int[] arr, int low, int high) {
+        
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+            
+            quickSort(arr, low, pivotIndex);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+        
+    }
+    
+    private int partition(int[] arr, int left, int right) {
+        
+        int pivot = arr[(left + right) / 2];
+        
+        left--;
+        right++;
+        
+        while (true) {
+            
+            do {
+                left++;
+            } while (arr[left] < pivot);
+            
+            do {
+                right--;
+            } while (arr[right] > pivot && left <= right);
+            
+            if (left >= right) {
+                return right;
+            }
+            
+            swap(arr, left, right);
+        }
+        
+    }
+    
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    
 }
