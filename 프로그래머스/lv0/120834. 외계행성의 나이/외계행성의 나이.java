3d9af13e3dbd7089c65e9ac4505676a988b6a0bb
samjan29 <78520058+samjan29@users.noreply.github.com>
2023-06-06 09:36:58 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.09 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,30 +1,12 @@
 class Solution {
     public String solution(int age) {
-        StringBuffer sb = new StringBuffer();
+        String[] strArray = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
         
-        while (age > 0) {
-            System.out.println(age);
-            if (age == 1000) {
-                sb.append("baaa");
-                age %= 1000;
-            } else if (age > 100) {
-                int temp = age / 100;
-                sb.append((char) ('a' + temp));
-                age -= temp * 100;
-            } else if (age == 100) {
-                sb.append("baa");
-                age %= 100;
-            } else if (age > 10) {
-                int temp = age / 10;
-                sb.append((char) ('a' + temp));
-                age -= temp * 10;
-            } else if (age == 10) {
-                sb.append("ba");
-                age %= 10;
-            } else {
-                sb.append((char) ('a' + age));
-                age = 0;
-            }
+        String strAge = Integer.toString(age);
+        StringBuffer sb = new StringBuffer(strAge.length());
+        
+        for (int i = 0; i < strAge.length(); i++) {
+            sb.append(strArray[strAge.charAt(i) - '0']);
         }
         
         return sb.toString();
