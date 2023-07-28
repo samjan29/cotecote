fe357db2f064193d4f44899a38b66cea3e034a8b
samjan29 <78520058+samjan29@users.noreply.github.com>
2023-07-28 23:32:19 +0900

    [level 2] Title: 기능개발, Time: 2.61 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> solution(int[] progresses, int[] speeds) {
+        Integer[] boxedProgress = Arrays.stream(progresses).boxed().toArray(Integer[]::new);
+        Queue<Integer> progressQueue = new LinkedList<>(Arrays.asList(boxedProgress));
+        
+        Integer[] boxedSpeed = Arrays.stream(speeds).boxed().toArray(Integer[]::new);
+        Queue<Integer> speedQueue = new LinkedList<>(Arrays.asList(boxedSpeed));
+        
+        List<Integer> answer = new ArrayList<>();
+        
+        int index = 0;
+        while (progressQueue.size() != 0) {
+            for (int i = index; i < progresses.length; i++) {
+                progresses[i] += speeds[i];
+            }
+            
+            if (progresses[index] >= 100) {
+                int count = 0;
+                for (int i = index; i < progresses.length; i++) {
+                    if (progresses[i] < 100) {
+                        break;
+                    }
+                    
+                    index++;
+                    count++;
+                    progressQueue.poll();
+                    speedQueue.poll();
+                }
+                
+                answer.add(count);
+            }
+        }
+        
+        return answer;
+    }
+}
