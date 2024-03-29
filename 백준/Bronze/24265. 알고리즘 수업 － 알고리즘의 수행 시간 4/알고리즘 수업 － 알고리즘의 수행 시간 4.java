4a8650b85ff2523e766fb6a1756d7a0a7c59d45e
samjan29 <78520058+samjan29@users.noreply.github.com>
2024-03-29 13:19:23 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 76 ms, Memory: 11512 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    /*
+        n <- 7
+        MenOfPassion(A[], n) {
+            sum <- 0;
+            for i <- 1 to n - 1     => O(6) => O(n-1)
+                for j <- i + 1 to n => O(6 + 5 + 4 + 3 + 2 + 1) => 등차수열 O(n(n-1)/2)
+                    sum <- sum + A[i] × A[j]; # 코드1
+            return sum;
+        }
+
+     */
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
+        
+        long count = n * (n - 1) / 2;
+        System.out.println(count);
+        int degree;
+        if (count >= Math.pow(n, 3)) {
+            degree = 4;
+        } else {
+            degree = 2;
+        }
+        System.out.println(degree);
+
+        br.close();
+    }
+
+}
