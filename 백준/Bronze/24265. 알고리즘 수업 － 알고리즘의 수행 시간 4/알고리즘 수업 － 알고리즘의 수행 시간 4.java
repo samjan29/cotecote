8fb88a6a446ee015ae24bacd8de24f001bf70993
samjan29 <78520058+samjan29@users.noreply.github.com>
2024-03-29 14:05:34 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 76 ms, Memory: 11492 KB -BaekjoonHub

--- 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -8,7 +8,7 @@ public class Main {
         n <- 7
         MenOfPassion(A[], n) {
             sum <- 0;
-            for i <- 1 to n - 1     => O(6) => O(n-1)
+            for i <- 1 to n - 1     => 6
                 for j <- i + 1 to n => O(6 + 5 + 4 + 3 + 2 + 1) => 등차수열 O(n(n-1)/2)
                     sum <- sum + A[i] × A[j]; # 코드1
             return sum;
@@ -23,8 +23,10 @@ public class Main {
         long count = n * (n - 1) / 2;
         System.out.println(count);
         int degree;
-        if (count >= Math.pow(n, 3)) {
+        if (count >= Math.pow(n, 4)) {
             degree = 4;
+        } else if (count >= Math.pow(n, 3)) {
+            degree = 3;
         } else {
             degree = 2;
         }
