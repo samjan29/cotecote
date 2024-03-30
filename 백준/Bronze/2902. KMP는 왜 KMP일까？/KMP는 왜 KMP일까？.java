6e5a68dc6a064a85e6c6a2d4030f1e8d36979b35
samjan29 <78520058+samjan29@users.noreply.github.com>
2024-03-30 10:38:51 +0900

    [Bronze II] Title: KMP는 왜 KMP일까?, Time: 124 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Bronze/2902. KMP는 왜 KMP일까？/KMP는 왜 KMP일까？.java
+++ 백준/Bronze/2902. KMP는 왜 KMP일까？/KMP는 왜 KMP일까？.java
@@ -1,9 +1,11 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        char[] arr = sc.nextLine().toCharArray();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] arr = br.readLine().toCharArray();
 
         StringBuilder sb = new StringBuilder();
         sb.append(arr[0]);  // 첫 글자는 항상 대문자
