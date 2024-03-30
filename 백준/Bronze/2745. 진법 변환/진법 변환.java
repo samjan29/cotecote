bc9583c87edf3becaa071045fff16fc7528eb2ad
samjan29 <78520058+samjan29@users.noreply.github.com>
2024-03-30 11:22:26 +0900

    [Bronze II] Title: 진법 변환, Time: 128 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        long answer = 0L;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] strArr = br.readLine().split(" ");
+        char[] charArr = strArr[0].toCharArray();
+        int n = Integer.parseInt(strArr[1]);
+
+        for (int i = 0; i < charArr.length; i++) {
+            int tmp = 0;
+            if (charArr[i] >= 'A' && charArr[i] <= 'Z') {
+                tmp = charArr[i] - 'A' + 10;
+            } else {
+                tmp = charArr[i] - '0';
+            }
+            answer += (long) (Math.pow(n, charArr.length - 1 - i) * tmp);
+        }
+
+        System.out.println(answer);
+        br.close();
+    }
+}
